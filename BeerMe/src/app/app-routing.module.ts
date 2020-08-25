e3d62a4932933966fef5888040a2a45a4b45e79a
emartin <emartint90@gmail.com>
2020-08-26 00:43:05 +0200

    Header creado y componente buscador (master)

--- BeerMe/src/app/app-routing.module.ts
+++ BeerMe/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BuscadorComponent } from './buscador/buscador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '' },
+  { path: 'buscador', component: BuscadorComponent },
+  { path: '**', redirectTo: '' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BeerMe/src/app/app.module.ts
+++ BeerMe/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeadComponent } from './head/head.component';
+import { FooterComponent } from './footer/footer.component';
+import { BuscadorComponent } from './buscador/buscador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeadComponent,
+    FooterComponent,
+    BuscadorComponent
   ],
   imports: [
     BrowserModule,
