6e6577f6c7a4555250537692e729c3efc4af981b
emartin <emartint90@gmail.com>
2020-09-15 12:42:39 +0200

    creada ruta (cervezasPais) en app-routing.module

--- BeerMe/src/app/app-routing.module.ts
+++ BeerMe/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   { path: 'buscador', component: BuscadorComponent },
   { path: 'home', component: HomeComponent },
   { path: 'cervezas/:cervezaNombre', component: VistaBuscadorComponent },
+  { path: 'cervezas/:cervezasPais', component: VistaBuscadorComponent },
   { path: '**', redirectTo: '/home' }
 
 
